d46298dff44e341bc9dec554dfe9261787e3fce2
Simon Allcorn <simon.allcorn@gmail.com>
2023-04-24 15:43:31 +0100

    tidy up

--- src/test/java/org/allcorn/iagDemo/model/IATATest.java
+++ src/test/java/org/allcorn/iagDemo/model/IATATest.java
@@ -1,22 +1,21 @@
 package org.allcorn.iagDemo.model;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class IATATest {
 
-    @ParameterizedTest
-    @ValueSource(strings = {"A", "AA", "ABCD", "ABCDE"})
-    public void rejectWrongLengthCodes(String incorrectLength) {
-        Assertions.assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() ->
-                IATA.of(incorrectLength));
-    }
+  @ParameterizedTest
+  @ValueSource(strings = {"A", "AA", "ABCD", "ABCDE"})
+  public void rejectWrongLengthCodes(String incorrectLength) {
+    Assertions.assertThatExceptionOfType(IllegalStateException.class)
+        .isThrownBy(() -> IATA.of(incorrectLength));
+  }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"ABC", "LHR", "LAX"})
-    public void successfullyCreateValidIATACodes(String validCode) {
-        Assertions.assertThatNoException().isThrownBy(() -> IATA.of(validCode));
-    }
+  @ParameterizedTest
+  @ValueSource(strings = {"ABC", "LHR", "LAX"})
+  public void successfullyCreateValidIATACodes(String validCode) {
+    Assertions.assertThatNoException().isThrownBy(() -> IATA.of(validCode));
+  }
 }
